863cd4aa20292ce48205a5e2ed2747e811ee4855
chulkov-alex <chulkov-alex@yandex-team.ru>
2025-02-17 12:06:36 +0300

    stud_league_helper.py

--- stud_league_helper.py
+++ stud_league_helper.py
@@ -39,6 +39,7 @@ def calculate_daily_points(current_results, previous_results):
     # Convert data to DataFrame for easy manipulation
     current_df = pd.DataFrame(current_results)
     previous_df = pd.DataFrame(previous_results)
+    previous_df['points_previous'] = previous_df['points_current']
     previous_df.drop(columns=['points_current'], inplace=True)
     current_df.columns = ['participant_name', 'points_current']
 
@@ -46,8 +47,8 @@ def calculate_daily_points(current_results, previous_results):
     merged_df = pd.merge(current_df, previous_df, on='participant_name', how="left")
     merged_df.fillna(0, inplace=True)
     # Calculate the difference in points
-    merged_df[today] = merged_df['points_current'] - merged_df[previous_df.columns.values[-1]]
-
+    merged_df[today] = merged_df['points_current'] - merged_df['points_previous']
+    merged_df.drop(columns=['points_previous'], inplace=True)
     # Select necessary columns
     # result_df = merged_df[['participant_name', 'points_increase']]
 
@@ -64,6 +65,7 @@ def update_csv_file(daily_points, csv_filename='tournament_results.csv'):
         # Append new data
         daily_points.fillna(0, inplace=True)  # Fill NaN values with 0 if new participants appear
         daily_points.to_csv(csv_filename, index=False)
+
 
 if __name__ == '__main__':
     current_results = fetch_tournament_data()
